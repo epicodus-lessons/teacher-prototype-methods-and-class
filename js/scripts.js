c2f94ef065f042082fe93828a1153fe653551e6b
patrick osten <patrickosten040@gmail.com>
2022-06-14 09:32:15 -0700

    parent and child class example for ES6 classes

--- js/scripts.js
+++ js/scripts.js
@@ -5,40 +5,45 @@
 ///******* */!!!
 
 //Business logic
-function Parent(name){//Parent constructor
-  //console.log(name) to see if the variable is coming in as what you expect
-  this.name = name;
-  //console.log(this.name) you can console.log() after to see if it got set right
-  this.child = new Child(name);
+class Parent {
+  constructor(name) {
+    this.name = name;
+    // this.child = new Child(name);// we can't create the child as a property of the Parent class in the same way, because it extends that parent class now and will gives us a recursive function call error https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Too_much_recursion
+  }
+  buyIceCream() {
+    return `${this.name} buys some ice cream`;
+  }
 }
-Parent.prototype.buyIceCream = function(){
-  return `${this.name} buys ice cream`;//template literal https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-}
-function Child(name){//Child constructor
-  this.name = "jr " + name;
-}
-Child.prototype.cries = function(){
-  return `${this.name} cries`
+
+class Child extends Parent {
+  constructor(name) {
+    super(name);
+    this.name = "jr " + name;
+  }
+  cries() {
+    return `${this.name} cries a lot`;
+  }
 }
 $(document).ready(function(){
   //setting my new parent variable outside the form submit scope so I can use it in other events (like the click listeners)
   let newParent; // don't need to set it equals to anything, it'll be undefined by default and we can change it later
+  let newChild;
   $('#result').text("the parent does not exist yet, please submit the form to create the parent")
   //event listeners below
   $('#name-form').submit(function(event){
     event.preventDefault();
-    const parentName = $('#p-name').val();//grab value from form input
-    newParent = new Parent(parentName);//create parent using Parent() constructor and the `new` keyword
+    const inputtedName = $('#p-name').val();//grab value from form input
+    newParent = new Parent(inputtedName);//create parent using Parent() constructor and the `new` keyword
+    newChild = new Child(inputtedName); 
     console.log(newParent);
     $('#result').text("your parent exists now, congratulations. Click one of the buttons below to begin parenting.")
   });
   $('#cry').click(function(){
-    $('#result').text(newParent.child.cries());//set the text to the return of Child.prototype.cries()
+    $('#result').text(newChild.cries());//set the text to the return of Child.cries(). The child can now also has buyIceCream as one of it's methods because it is extending the Parent class, so it inherits the parents methods.
+    console.log(newChild.buyIceCream());//look in the console to see this output
   });
   $('#buy').click(function(){
-    $('#result').text(newParent.buyIceCream());//set the text to the return of Parent.prototype.buyIceCream()
+    //console.log(newParent.cries());//<-- this is not a funciton. The Parent object does not have cry() as one of it's methods, that method is defined in the Child class. Even though the Child inherits the Parents methods, the reverse it not true and the Parent does not inherit the childs methods.
+    $('#result').text(newParent.buyIceCream());
   });
-  //NOTE  
-  //newParent.cries() is not a funciton, cries only belongs to the Child object. Even though the child object exists as a property of the newParent, newParent does not inherit cries() as it's own method. 
-  //the same goes for newParent.child.buyIceCream(), <-- that is not a function. The Child inside newParent dosen't have that method as part of it's prototype
 })
