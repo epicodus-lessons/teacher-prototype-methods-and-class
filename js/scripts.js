7e2d67e1b8cb4dffd36e89a43dc79c1b621d1c88
patrick osten <patrickosten040@gmail.com>
2022-06-16 12:14:12 -0700

    clean up comments and make match main branch

--- js/scripts.js
+++ js/scripts.js
@@ -2,42 +2,64 @@
 class Parent {
   constructor(name) {
     this.name = name;
-    // this.child = new Child(name);// we can't create the child as a property of the Parent class in the same way, because Child extends that parent class now and will gives us a recursive function call error https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Too_much_recursion
-  }
+//We can't have the Child as a property 
+//of the Parent class because Child extends Parent now. 
+//Why exactly that is a bit beyond the scope of this example,
+//and you do not need to know it. Just know that if a class 
+//extends another class, it gets access to all the parent classes methods.
+//But if you're curious see this page https://javascript.info/class-inheritance
+}
   buyIceCream() {
-    return `${this.name} buys some ice cream`;
+    return this.name +  ' buys some ice cream';
   }
 }
 
 class Child extends Parent {
   constructor(name) {
     super(name);
-    this.name = "jr " + name;
+    this.name = name + ' jr';
   }
   cries() {
-    return `${this.name} cries a lot`;
+    return this.name + ' cries a lot';
   }
 }
 $(document).ready(function(){
-  //setting my new parent variable outside the form submit scope so I can use it in other events (like the click listeners below)
-  let newParent; // don't need to set it equal to anything, it'll be undefined by default and we can change it later
+  let newParent;
   let newChild;
-  $('#result').text("the parent does not exist yet, please submit the form to create the parent")
-  //event listeners below
+//event listeners below
   $('#name-form').submit(function(event){
     event.preventDefault();
-    const inputtedName = $('#p-name').val();//grab value from form input
-    newParent = new Parent(inputtedName);//create parent using Parent() constructor and the `new` keyword
+    $('.hidden').show();
+    const inputtedName = $('#p-name').val();
+    newParent = new Parent(inputtedName);
     newChild = new Child(inputtedName); 
     console.log(newParent);
-    $('#result').text("your parent exists now, congratulations. Click one of the buttons below to begin parenting.")
+    $('#child-name').text(newParent.name);
+    $('#parent-name').text(newChild.name);
   });
   $('#cry').click(function(){
-    $('#result').text(newChild.cries());//set the text to the return of Child.cries(). The child can now also has buyIceCream as one of it's methods because it is extending the Parent class, so it inherits the parents methods.
-    console.log(newChild.buyIceCream());//look in the console to see this output
+    $('#child-output').text(newChild.cries());
+  });
+  $('#child-buy').click(function(){
+    $('#child-output').text(newChild.buyIceCream());
+//The child can now also has buyIceCream as one of 
+//it's methods because it is extending the Parent class,
+//so it inherits the parents methods.
   });
   $('#buy').click(function(){
-    //console.log(newParent.cries());//<-- this is not a funciton. The Parent object does not have cry() as one of it's methods, that method is defined in the Child class. Even though the Child inherits the Parents methods, the reverse it not true and the Parent does not inherit the childs methods.
-    $('#result').text(newParent.buyIceCream());
+    $('#parent-output').text(newParent.buyIceCream());
+//newParent.cries();//<-- this is not a funciton. 
+//The Parent object does not have cry() as one of it's methods,
+//that method is defined in the Child class. Even though the Child 
+//inherits the Parents methods, the reverse it not true and the 
+//Parent does not inherit the childs methods.
   });
+  $('#reset-everything').click(function(){
+    $('.hidden').hide();
+    $('#p-name').val("");
+    $('#child-name').text('Child name does not exist, because Child object does not exist yet');
+    $('#parent-name').text('parent name does not exist, because parent object does not exist yet');
+    $('#parent-output').text('');
+    $('#child-output').text('');
+  })
 })
